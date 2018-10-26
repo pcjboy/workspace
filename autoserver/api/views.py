5ada6f4066be9dd3f241861ac2a3ab2969df7ede
pcjboy <pcjboy@gamil.com>
2018-10-26 16:17:19 +0800

    1
    1

--- autoserver/api/views.py
+++ autoserver/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+
+def aseet(request):
+    if request.method == 'POST':
+
+        print(request.method)
+        return HttpResponse("OK")
+
--- autoserver/autoserver/urls.py
+++ autoserver/autoserver/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, url, include
+from django.conf.urls import url, include
+from api import views
 
 urlpatterns = [
-    path('api/', ),
+    url(r'asset.html$', views.aseet),
 ]
