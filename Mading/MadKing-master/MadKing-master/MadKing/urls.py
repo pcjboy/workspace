558c9317e36a2fccf31e10cd789b1e9ff65edc8c
pcjboy <pcjboy@gamil.com>
2019-02-28 20:36:25 +0800

    1
    1

--- Mading/MadKing-master/MadKing-master/MadKing/urls.py
+++ Mading/MadKing-master/MadKing-master/MadKing/urls.py
@@ -19,10 +19,9 @@ from assets import rest_urls, urls as asset_urls
 from MadKing import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(rest_urls)),
     url(r'^asset/', include(asset_urls)),
     url(r'^$', views.index, name="dashboard"),
     url(r'^login/$', views.acc_login, name='login'),
-
 ]
--- Mading/MadKing-master/MadKing-master/MadKingClient/conf/settings.py
+++ Mading/MadKing-master/MadKing-master/MadKingClient/conf/settings.py
@@ -5,8 +5,8 @@ import os
 BaseDir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 Params = {
-    "server": "192.168.1.38",
-    "port": 9000,
+    "server": "10.21.10.180",
+    "port": 8000,
     'request_timeout': 30,
     "urls": {
         "asset_report_with_no_id": "/asset/report/asset_with_no_asset_id/",
